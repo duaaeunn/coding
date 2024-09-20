c6f2dce080b87b92ec8cb58616f5956e0dd5e27d
duaaeunn <daeunj1532@icloud.com>
2024-09-20 16:47:30 +0900

    Update test3.py

--- views/test3.py
+++ views/test3.py
@@ -13,8 +13,8 @@ def playGame(guessNum):
 st.title("동전 던지기 게임")
 st.divider()
 
-st.image('assets/coin_head.png.')
-st.image('assets/coin_tail.png.')
+st.image('assets/coin_head.png')
+st.image('assets/coin_tail.png')
 
 st.header('동전 던지기 게임에 오신 것을 환영합니다.')
 st.subheader('앞면일까요? 뒷면일까요?')
